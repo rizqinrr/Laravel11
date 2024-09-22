cc2a23246d11d591f48160a4a891192fadf16522
rizqinrr <studyrizqi@gmail.com>
2024-09-22 14:24:38 +0700

    First Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">About Me</h3>
+  <h3>Owner : {{$nama}}</h3>
+  <img src="img/r1.jpg" alt="R1" width="200">
+
+</x-layout>
+
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">Contact Me</h3>
+  <ul>
+    <li>
+      Instagram
+    </li>
+    <li>
+      LinkedIn
+    </li>
+  </ul>
+</x-layout>
