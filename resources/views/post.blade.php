2c68bb71f8b1dac049fb4e65c07058eb224ae17a
rizqinrr <studyrizqi@gmail.com>
2024-09-30 23:24:14 +0700

    Post Category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,16 +2,27 @@
     <x-slot:title>{{ $title }}</x-slot:title>
 
 
-        <article class="py-8 max-w-screen-md">
+    <article class="py-8 max-w-screen-md">
 
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
 
-            <div class="text-base text-gray-500">
-                <a href="/author/{{ $post->author->id }}">{{ $post->author->name; }}</a> | {{ $post->created_at->diffForHumans() }}
-            </div>
-            <p class="my-4 font-light">{{ $post['body'] }}</p>
+        <div>
+            By <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">
+                {{ $post->author->name }}
+            </a>
+            in <a href="/categories/{{ $post->category->slug }}"
+                class="text-base text-gray-500 hover:underline">{{ $post->category->name }}</a>
+            | {{ $post->created_at->diffForHumans() }}
+        </div>
+        <p class="my-4 font-light">{{ $post['body'] }}</p>
 
-            <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
-        </article>
+        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
+    </article>
 
 </x-layout>
+
+{{-- kategory 
+1. Olahraga
+2. Alam
+3. Budaya
+4. Sosial --}}
